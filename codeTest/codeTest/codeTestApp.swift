9c41491285b63dc1d89016315fc22da55a730ffa
marilise <marilise.morona@prestadores.intermedica.com.br>
2023-11-06 09:50:48 -0300

    Create a project

---
+++ codeTest/codeTest/codeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  codeTestApp.swift
+//  codeTest
+//
+//  Created by marilise morona on 06/11/23.
+//
+
+import SwiftUI
+
+@main
+struct codeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
